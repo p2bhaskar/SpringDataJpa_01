123e944bcd4e563f76cdb13872e15b2464c6a9c8
Pranav <p2bhaskar@gmail.com>
2023-11-23 13:59:34 +0530

    Upgraded SpringDataJpa project to SpringBoot-3 suceessfully

---
+++ src/main/java/com/javatechie/entity/Product.java
@@ -0,0 +1,24 @@
+package com.javatechie.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+    //@Column(name = "DESC")
+    private String description;
+    private String productType;
+}
